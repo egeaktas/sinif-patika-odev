7df5cbbe2b9f9d7a8bc0d894c6492e50e360888f
Ege Aktas <egeaktas12@gmail.com>
2022-09-27 21:24:40 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double mat, fiz, turk, kim, muzik, toplam = 0, sayac = 0, ort;
+        
+        mat = input.nextDouble();
+        fiz = input.nextDouble();
+        turk = input.nextDouble();
+        kim = input.nextDouble();
+        muzik = input.nextDouble();
+
+        if(mat>=0 && mat<= 100){
+            toplam = toplam + mat;
+            ++sayac;
+        }
+
+        if(fiz>=0 && fiz<= 100){
+            toplam = toplam + fiz;
+            ++sayac;
+        }
+
+        if(turk>=0 && turk<= 100){
+            toplam = toplam + turk;
+            ++sayac;
+        }
+
+        if(kim>=0 && kim<= 100){
+            toplam = toplam + kim;
+            ++sayac;
+        }
+
+        if(muzik>=0 && muzik<= 100){
+            toplam = toplam + muzik;
+            ++sayac;
+        }
+
+        ort = toplam / sayac;
+
+        System.out.println(ort);
+
+    }
+}
